fc49d37478c704f3cb9839b40f26f5909187a96e
brherr <herrbrad@yahoo.com>
2025-02-24 12:50:07 -0500

    interceptors

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import authFetch from "../axios/interceptors";
 
-const url = 'https://www.course-api.com/react-store-products';
+const url = "https://www.course-api.com/react-store-products";
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    const response = await authFetch("/react-store-products");
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+  return <h2 className="text-center">interceptors</h2>;
 };
 export default Interceptors;
